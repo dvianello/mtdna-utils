77af5961ac59bcf727281b145c79e37f8f7e883c
Dario Vianello <dario.vianello@unibo.it>
2013-09-11 12:31:27 +0200

    Add inner workings. Add docstrings.

--- extract_subsequence.py
+++ extract_subsequence.py
@@ -1,41 +1,132 @@
 #!/usr/bin/python
-__author__ = 'Dario Vianello'
+# coding=utf-8
+"""
+A script to extract identical subsequences starting from sequences in FASTA format.
 
-import sys
+Usage:
+
+    extract_subsequence.py --fasta_file test_files/test_sequences.fasta 10 20
+
+    This command will extract the range nucleotides from position 10 to 20 from all the sequences
+    contained in the fasta file test_sequences.fasta .
+
+
+    If you need to extract a subsequence from a given start position to the end of the seq, simply specify only
+    the starting point:
+
+    extract_subsequence.py --fasta_file test_files/test_sequences.fasta 10
+
+
+    You can combine more than a subsequence in a single, composite, subsequence:
+
+    extract_subsequence.py --fasta_file test_files/test_sequences.fasta 10 20 30 40
+
+    this will output a single subsequence composed by the subsequences 10-20 30-40 for each sequence in the FASTA file.
+
+
+    If end position is smaller than start position, the script will assume that you are trying to extract a subsequence
+    that encompasses the end of the mtDNA molecule (e.g. Dloop). In that case, the program will combine two "virtual"
+    subsequences:
+
+            start_point-endofsequence & startofsequence-end_point
+
+    Obviously, also in this case more than a subsequence can be extracted at a time.
+
+
+"""
+
 import argparse
 
-#This tool need BioPython library
-from Bio import SeqIO
+#This tool needs BioPython library
+from Bio import SeqIO, SeqRecord, Seq
 
 
-def extractDloop(sequence, startPosition, endPosition):
+def extractAroundEnd(sequence, startPosition, endPosition):
+    """
+    Extract a subsequence that encompasses the sequence end.
+
+
+    :param sequence: Parent sequence
+    :type sequence: SeqObject
+    :param startPosition: Subsequence start position (should be greater than endPosition)
+    :type startPosition: int
+    :param endPosition: Subsequence end position (should be smaller than startPosition
+    :type endPosition: int
+
+    :returns: Subsequence of parent sequence
+    :rtype: SeqObject
+
+    """
+
     return sequence[startPosition - 1:] + sequence[:endPosition]
 
 
-def extractSubsequence(sequence, startPosition, endPosition):
-    return sequence[startPosition - 1:endPosition]
+def extractSubsequence(sequence, startPosition, endPosition=None):
+    """
+    Extract a subsequence.
 
 
-parser = argparse.ArgumentParser(prog="extract_subsequence",
-                                 description="Extract a subsequence from a set of sequences, eventually combining more \
-                                 than one subsequence")
+    :param sequence: Parent sequence
+    :type sequence: SeqObject
+    :param startPosition: Subsequence start position (must be smaller than endPosition)
+    :type startPosition: int
+    :param endPosition: Subsequence end position (must be smaller than startPosition
+    :type endPosition: int
 
-parser.add_argument('--fasta_file', nargs=1, type=file, required=True, help="FASTA file to be processed")
-parser.add_argument('subsequences', nargs='+', help="Subsequences to be extracted and joined")
-args = parser.parse_args()
-print args
+    :returns: Subsequence of parent sequence
+    :rtype: SeqObject
 
-raise SystemExit
-
-f = open(sys.argv[1], 'r')
-output = open('dloops.fasta', 'w')
-sequences = SeqIO.parse(f, 'fasta')
-for items in sequences:
-    print >> output, '>' + items.id
-    if endPosition < startPosition:
-        print >> output, extractDloop(items.seq, startPosition, endPosition)
+    """
+    if endPosition:
+        return sequence[startPosition - 1:endPosition]
     else:
-        print >> output, extractSubsequence(items.seq, startPosition, endPosition)
+        return sequence[startPosition - 1:]
 
-f.close()
+
+# Argument parser declaration
+parser = argparse.ArgumentParser(prog='extract_subsequence',
+                                 description='Extract a subsequence from a set of sequences, eventually combining more \
+                                 than one subsequence',
+)
+
+parser.add_argument('--fasta_file', nargs=1, type=file, required=True, help='FASTA file to be processed')
+parser.add_argument('--output_file', nargs=1, required=True, help='Output file')
+parser.add_argument('subsequences', nargs='+', type=int, help='Subsequences to be extracted and joined. Eg: 10 20')
+args = parser.parse_args()
+
+# Open handle for output
+output = open(args.output_file[0], 'w+')
+
+sequences = SeqIO.parse(args.fasta_file[0], 'fasta')
+subsequences_length = len(args.subsequences)
+
+# If len requestes subsequences is odd, the last subsequence must
+# go to the end of the seq
+if len(args.subsequences) % 2 != 0:
+    last_subseq_start = args.subsequences.pop(len(args.subsequences) - 1)
+else:
+    last_subseq_start = None
+
+for seq in sequences:
+    subseqs = []
+
+    for i in xrange(0, len(args.subsequences), 2):
+        start_position, end_position = args.subsequences[i], args.subsequences[i + 1]
+
+        if end_position < start_position:
+            subseqs.append(extractAroundEnd(seq.seq, start_position, end_position))
+        else:
+            subseqs.append(extractSubsequence(seq.seq, start_position, end_position))
+
+    if last_subseq_start:
+        subseqs.append(extractSubsequence(seq.seq, last_subseq_start))
+
+    merged_seq = SeqRecord.SeqRecord(Seq.Seq(''), id=seq.id, description=seq.description)
+
+    for subseq in subseqs:
+        merged_seq += subseq
+
+    SeqIO.write(merged_seq, output, 'fasta')
+
+args.fasta_file[0].close()
 output.close()
